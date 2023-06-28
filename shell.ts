bf33876b05aebd12ed99056358e7aaf97b153cd9
romulo <romulo@mindsea.com>
2023-06-27 21:19:22 -0400

    Created Domain, Data and Api folders. Moved Models folder to domain.

--- shell.ts
+++ shell.ts
@@ -1,7 +1,7 @@
 // $ npx ts-node shell.ts
 import repl from 'node:repl';
 import dotenv from 'dotenv';
-import db from './src/models';
+import db from './src/domain/models';
 dotenv.config();
 
 const r = repl.start('> ');
