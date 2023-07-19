47e8889da5a1c9359c909ccb5f6d31caacc63ce0
romulo <romulo@mindsea.com>
2023-07-18 21:27:13 -0400

    Created endpoint test POST /api/v1/users/account/signin.

---
+++ test/api/v1/users/account/user-account-router.test.ts
@@ -0,0 +1,34 @@
+import request from "supertest";
+import app from "../../../../../src/app";
+import db from "../../../../../src/data/models";
+import userFactory from "../../../../factory/user-factory";
+import PasswordUtils from "../../../../../src/domain/utils/password-utils";
+
+describe('UserAccountRouter', () => {
+  test('Success POST /api/v1/users/account/signin', async() => {
+    const { User, ApiKey } = db.models;
+
+    const userPassword = '12341234';
+    const user = await User.create(userFactory({
+      password: PasswordUtils.createHash(userPassword)
+    }));  
+
+    const userJSON = user.toJSON();
+
+    const payload = {
+      email: userJSON.email, 
+      password: userPassword
+    };
+
+    const response = await request(app)
+      .post('/api/v1/users/account/signin')
+      .set('Accept', 'application/json')
+      .send(payload);
+
+    expect(response.status).toEqual(200);
+    expect(response.body.apikey).not.toBeNull();
+
+    await ApiKey.destroy({ where: { userId: userJSON.id }});
+    await user.destroy();
+  });
+});
